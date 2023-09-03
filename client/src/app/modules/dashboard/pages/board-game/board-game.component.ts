846dcb0e4923f770e50b74cfbb1c6f09675cd61a
Argyro <arguro_s@outlook.com>
2023-09-03 15:06:50 +0300

    Adding dashboard component

---
+++ client/src/app/modules/dashboard/pages/board-game/board-game.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-game',
+  templateUrl: './board-game.component.html',
+  styleUrls: ['./board-game.component.css']
+})
+export class BoardGameComponent {
+
+}
